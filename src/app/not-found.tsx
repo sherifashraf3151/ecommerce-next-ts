ce8235ddb71d49d0b0f29f699ea8bd656935ca71
sherifashraf3151 <sherifashraf00000@gmail.com>
2026-01-31 22:01:08 +0200

    Not Found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function NotFound() {
   return (
-    <div className='min-h-screen flex justify-center items-center'>
-      <h1 className='text-4xl font-bold'>404 - Page Not Found</h1>
-    </div>
+    <>
+      <div className="text-center animate-fadeIn mt-20">
+        <Image src="/404.png" alt="404 Illustration" className="mx-auto w-80 animate-[float_3s_infinite] shadow-xl rounded-lg" width={300} height={300}/>
+          <h1 className="text-7xl font-extrabold text-blue-700 mt-6">Looks Like You`re Lost!</h1>
+          <p className="text-xl text-gray-700 mt-2">We can`t seem to find the page you`re looking for.</p>
+          <Link href="/" className="mt-6 inline-block bg-blue-600 text-white px-8 py-3 rounded-full text-lg font-semibold shadow-lg transform transition hover:scale-105 hover:bg-blue-700">Return Home</Link>
+      </div>
+    </>
   )
 }
