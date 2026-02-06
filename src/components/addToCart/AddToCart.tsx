372a05e513175f1a1e72ff52ef0e558e1f7d3dbe
sherifashraf3151 <sherifashraf00000@gmail.com>
2026-02-06 18:12:35 +0200

    fix

--- src/components/addToCart/AddToCart.tsx
+++ src/components/addToCart/AddToCart.tsx
@@ -4,10 +4,13 @@ import { Button } from '../ui/button'
 import { Loader, ShoppingCartIcon } from 'lucide-react'
 import toast from 'react-hot-toast';
 import { CartContext } from '../context/cartContext';
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5N2RjNGI5MzI2MzUwYmE3NTcyN2NiNCIsIm5hbWUiOiJTaGVyaWYgYXNocmFmIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NzAwNjE1MjAsImV4cCI6MTc3NzgzNzUyMH0.uxLQdBVV3_uuh9cTUxbH2tRtG6FSVgSNSiUJyCC0G0w
 
-// productId is Props Coming From ProductId and Proucts Page To Know Which Product to Add to Cart
-export default function AddToCart({ productId }: { productId: string }) {
+interface AddToCartProps {
+  productId: string;
+  className?: string; // <-- هنا ضفنا className اختياري
+}
+
+export default function AddToCart({ productId, className }: AddToCartProps) {
 
   const { getCart, setCartData } = useContext(CartContext)
 
@@ -15,25 +18,33 @@ export default function AddToCart({ productId }: { productId: string }) {
 
   async function addProductToCart() {
     setIsLoading(true);
-    const response = await fetch('https://ecommerce.routemisr.com/api/v1/cart', {
-      method: 'POST',
-      // JSON Because The API Accept JSON Format
-      body: JSON.stringify({ productId }),
-      headers: {
-        token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5N2RjNGI5MzI2MzUwYmE3NTcyN2NiNCIsIm5hbWUiOiJTaGVyaWYgYXNocmFmIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NzAwNjE1MjAsImV4cCI6MTc3NzgzNzUyMH0.uxLQdBVV3_uuh9cTUxbH2tRtG6FSVgSNSiUJyCC0G0w',
-        // Make Sure The Body Is JSON Format
-        'Content-Type': 'application/json'
-      }
-    })
-    const data = await response.json();
-    data.status == 'success' && toast.success('Product Added To Cart');
-    // await getCart()
-    setCartData(data)
-    setIsLoading(false);
+    try {
+      const response = await fetch('https://ecommerce.routemisr.com/api/v1/cart', {
+        method: 'POST',
+        body: JSON.stringify({ productId }),
+        headers: {
+          token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5N2RjNGI5MzI2MzUwYmE3NTcyN2NiNCIsIm5hbWUiOiJTaGVyaWYgYXNocmFmIiwicm9sZSI6InVzZXIiLCJpYXQiOjE3NzAwNjE1MjAsImV4cCI6MTc3NzgzNzUyMH0.uxLQdBVV3_uuh9cTUxbH2tRtG6FSVgSNSiUJyCC0G0w',
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await response.json();
+      if (data.status === 'success') toast.success('Product Added To Cart');
+      setCartData(data)
+    } catch (error) {
+      toast.error('Failed to add product')
+    } finally {
+      setIsLoading(false);
+    }
   }
 
-
   return (
-    <Button disabled={isLoading} onClick={addProductToCart} className="grow  cursor-pointer"> {isLoading ? <Loader className='animate-spin' /> : <ShoppingCartIcon />} Add To Cart</Button>
+    <Button
+      disabled={isLoading}
+      onClick={addProductToCart}
+      className={`grow cursor-pointer ${className || ""}`} // <-- دمج className الممرر
+    >
+      {isLoading ? <Loader className='animate-spin' /> : <ShoppingCartIcon />}
+      Add To Cart
+    </Button>
   )
 }
